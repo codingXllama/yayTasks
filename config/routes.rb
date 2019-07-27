49894674150c473668adb014ffc8bd0191df16ea
codingxllama <osamaalchalabi2@gmail.com>
2019-07-26 19:43:54 -0600

    Changed route of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  # get 'pages/home'
+
+  # making the home pages to be the root 
+  # when we use #, that means it's an action
+  # so we are interested in the home action by doing #home
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
